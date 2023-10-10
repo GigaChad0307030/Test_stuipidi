33828060452f3b7c026997ed79d77bbee876c367
GigaChad0307030 <alexandru.nazare@students.uniroma2.eu>
2023-10-10 16:13:24 +0200

    cambiato

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,20 +1,17 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
+
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloController extends ControllerBase{
 
-    @FXML
-    private Label welcomeText;
+
     @FXML
     private AnchorPane anchorPane;
     @FXML
@@ -28,7 +25,7 @@ public class HelloController extends ControllerBase{
         a.setStage((Stage) anchorPane.getScene().getWindow(),"hello-view.fxml","test.fxml",true);
     }
     @FXML
-    protected void addClick() throws IOException {
+    protected void addClick() {
                 int r=Integer.parseInt(label_h.getText())+1;
                 label_h.setText(String.valueOf(r));
     }
--- src/main/java/com/example/demo/Testcontroller.java
+++ src/main/java/com/example/demo/Testcontroller.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
+
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
